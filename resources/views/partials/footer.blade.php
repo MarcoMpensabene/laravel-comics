317fa752f0cd17cd38511a236a168f59b6e8fb9d
MarcoMpensabene <marcompensabene@gmail.com>
2024-07-15 16:58:19 +0200

    added footer structure and footer scss

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,65 @@
 <footer>
+    <section>
+        <div>
+            <h2>dc comics</h2>
+            <ul>
+                <li  >
+                    <a href=""></a>
+                </li>
+            </ul>
+            <h2>shop</h2>
+            <ul>
+                <li  >
+                    <a href=""></a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h2>dc</h2>
+            <ul>
+                <li  >
+                    <a href=""></a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h2>sites</h2>
+            <ul>
+                <li  >
+                    <a href=""></a>
+                </li>
+            </ul>
+        </div>
+        <div class="dc-logo">
+            <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="logo-dc">
+        </div>
+    </section>
 
+    <nav>
+        <div class="footer-nav">
+            <button>
+                SIGN-UP NOW!
+            </button>
+            <ul class="icons">
+                <li >
+                    <h2 id="follow">Follow Us</h2>
+                </li>
+                <li>
+                    <img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="">
+                </li>
+                <li>
+                    <img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="">
+                </li>
+                <li>
+                    <img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="">
+                </li>
+                <li>
+                    <img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="">
+                </li>
+                <li>
+                    <img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="">
+                </li>
+            </ul>
+        </div>
+    </nav>
 </footer>
