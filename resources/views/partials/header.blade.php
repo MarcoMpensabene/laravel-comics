16c41d7a45f04c925cf6f00d7a4f61dd57f4a5e2
MarcoMpensabene <marcompensabene@gmail.com>
2024-07-15 16:43:17 +0200

    modified readme , added img to resources , added header scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,13 @@
 <header>
+    <nav>
 
+        <div class="header-logo">
+            <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="DC-Logo">
+        </div>
+        <ul>
+            <li>
+                <a href=>TEST</a>
+            </li>
+        </ul>
+        </nav>
 </header>
